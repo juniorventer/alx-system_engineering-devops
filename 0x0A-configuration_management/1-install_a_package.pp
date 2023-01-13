a86e7ab1b66f2c44275eb8804a173ff30302f77f
junior-venter <roccovent12@gmail.com>
2023-01-12 18:04:52 -0800

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,5 @@
-# Install a package using Puppet
-
-exec { 'install flask':
-  command => '/usr/bin/apt-get install -y flask'
-}
-
-exec { 'install puppet-lint':
-  command => '/usr/bin/gem install puppet-lint -v 2.1.1'
-}
-
-package { 'flask':
-  ensure => 'installed',
-  before => Exec['install flask']
-}
-
+# Install puppet lint
 package { 'puppet-lint':
-  ensure  => 'installed',
-  before  => Exec['install puppet-lint'],
-  require => Package['flask']
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
